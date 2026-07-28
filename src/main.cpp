48299a96910b89d0d77aa5e6702989344351e4b2
diazsd <diazsd@iu.edu>
2026-07-28 18:21:58 -0400

    combined transforms into one matrix

--- src/main.cpp
+++ src/main.cpp
@@ -6,51 +6,7 @@
 #include "utils.hpp"
 #include "constants.hpp"
 
-using std::vector, std::min, std::max, std::abs, std::tuple, std::sin, std::cos;
-
-void DrawLine(vector<Color>& pixels, float x0, float y0, float x1, float y1, Color color) {
-    const float dy = y1 - y0, dx = x1 - x0;
-    const int steps = max(abs(dx), abs(dy));
-
-    if (steps == 0) {
-        SetPixel(pixels, x0, y0, color);
-        return;
-    }
-
-    const float ystep = dy / steps;
-    const float xstep = dx / steps;
-
-    float y = y0, x = x0;
-    for (int i = 0; i <= steps; i++) {
-        SetPixel(pixels, x, y, color);
-        y += ystep;
-        x += xstep;
-    }
-}
-
-void DrawLine(vector<Color>& pixels, Point p1, Point p2, Color color) {
-    DrawLine(pixels, p1.x, p1.y, p2.x, p2.y, color);
-}
-
-float EdgeFunction(float ax, float ay, float bx, float by, float px, float py) {
-    return (px - ax) * (by - ay) - (py - ay) * (bx - ax);
-}
-
-float EdgeFunction(Point a, Point b, Point c) {
-    return EdgeFunction(a.x, a.y, b.x, b.y, c.x, c.y);
-}
-
-tuple<bool, float, float, float> InsideTriangle(Triangle t, int px, int py) {
-    const Vec3 a = t.a.position, b = t.b.position, c = t.c.position;
-    const float e0 = EdgeFunction(a.x, a.y, b.x, b.y, px, py);
-    const float e1 = EdgeFunction(b.x, b.y, c.x, c.y, px, py);
-    const float e2 = EdgeFunction(c.x, c.y, a.x, a.y, px, py);
-
-    const bool inside = (e0 >= 0 && e1 >= 0 && e2 >= 0) || (e0 <= 0 && e1 <= 0 && e2 <= 0);
-    const float area = EdgeFunction(a.x, a.y, b.x, b.y, c.x, c.y);
-    if (area == 0 || !inside) return tuple{false, 0, 0, 0};
-    return { inside, e1 / area, e2 / area, e0 / area };
-}
+using std::vector, std::min, std::max, std::sin, std::cos;
 
 void DrawTriangle(vector<Color>& pixels, Triangle t) {
     const Vec3 a = t.a.position, b = t.b.position, c = t.c.position;
@@ -71,14 +27,6 @@ void DrawTriangle(vector<Color>& pixels, Triangle t) {
     }
 }
 
-Point Centroid(Triangle t) {
-    float x = t.a.position.x + t.b.position.x + t.c.position.x;
-    float y = t.a.position.y + t.b.position.y + t.c.position.y;
-    x /= 3;
-    y /= 3;
-    return { x, y };
-}
-
 Vertex ApplyTransform (Vertex v, Transform transform) {
     Vec3 pos = v.position;
     return {
@@ -95,30 +43,33 @@ Triangle ApplyTransform(Triangle t, Transform transform) {
     return {avex, bvex, cvex };
 }
 
-Triangle Translate(Triangle t, float x, float y) {
-    Transform transform {
-        {1, 0, x},
-        {0, 1, y},
-        {0, 0, 1}
+Transform Translation(float x, float y) {
+    return {
+            {1, 0, x},
+            {0, 1, y},
+            {0, 0, 1}
     };
-
-    return ApplyTransform(t, transform);
 }
 
-float DegToRad(float degrees) {
-    return degrees * 0.0174533f;
-}
-
-Triangle Rotate(Triangle t, Point pivot, float radians) {
-    t = Translate(t, -pivot.x, -pivot.y);
-
-    Transform rotation {
+Transform Rotation(float radians) {
+    return {
             {cos(radians), -sin(radians), 0},
             {sin(radians), cos(radians), 0},
             {0, 0, 1},
         };
-    t = ApplyTransform(t, rotation);
-    return Translate(t, pivot.x, pivot.y);
+}
+
+Triangle Translate(Triangle t, float x, float y) {
+    return ApplyTransform(t, Translation(x, y));
+}
+
+Triangle Rotate(Triangle t, Point pivot, float radians) {
+    Transform to_pivot = Translation(-pivot.x, -pivot.y);
+    Transform rotate = Rotation(radians);
+    Transform to_original = Translation(pivot.x, pivot.y);
+    Transform transform = Multiply(to_original, Multiply(rotate, to_pivot));
+
+    return ApplyTransform(t, transform);
 }
 
 Triangle Rotate(Triangle t, float radians) {
--- src/utils.hpp
+++ src/utils.hpp
@@ -24,3 +24,61 @@ inline void SetPixel(std::vector<Color>& pixels, const int x, const int y, const
 inline void SetPixel(std::vector<Color>& pixels, const Point p, const Color color) {
     SetPixel(pixels, p.x, p.y, color);
 }
+
+Point Centroid(Triangle t) {
+    float x = t.a.position.x + t.b.position.x + t.c.position.x;
+    float y = t.a.position.y + t.b.position.y + t.c.position.y;
+    x /= 3;
+    y /= 3;
+    return { x, y };
+}
+
+float DegToRad(float degrees) {
+    return degrees * 0.0174533f;
+}
+
+using std::vector, std::max, std::abs;
+void DrawLine(vector<Color>& pixels, float x0, float y0, float x1, float y1, Color color) {
+    const float dy = y1 - y0, dx = x1 - x0;
+    const int steps = max(abs(dx), abs(dy));
+
+    if (steps == 0) {
+        SetPixel(pixels, x0, y0, color);
+        return;
+    }
+
+    const float ystep = dy / steps;
+    const float xstep = dx / steps;
+
+    float y = y0, x = x0;
+    for (int i = 0; i <= steps; i++) {
+        SetPixel(pixels, x, y, color);
+        y += ystep;
+        x += xstep;
+    }
+}
+
+void DrawLine(vector<Color>& pixels, Point p1, Point p2, Color color) {
+    DrawLine(pixels, p1.x, p1.y, p2.x, p2.y, color);
+}
+
+float EdgeFunction(float ax, float ay, float bx, float by, float px, float py) {
+    return (px - ax) * (by - ay) - (py - ay) * (bx - ax);
+}
+
+float EdgeFunction(Point a, Point b, Point c) {
+    return EdgeFunction(a.x, a.y, b.x, b.y, c.x, c.y);
+}
+
+using std::tuple;
+tuple<bool, float, float, float> InsideTriangle(Triangle t, int px, int py) {
+    const Vec3 a = t.a.position, b = t.b.position, c = t.c.position;
+    const float e0 = EdgeFunction(a.x, a.y, b.x, b.y, px, py);
+    const float e1 = EdgeFunction(b.x, b.y, c.x, c.y, px, py);
+    const float e2 = EdgeFunction(c.x, c.y, a.x, a.y, px, py);
+
+    const bool inside = (e0 >= 0 && e1 >= 0 && e2 >= 0) || (e0 <= 0 && e1 <= 0 && e2 <= 0);
+    const float area = EdgeFunction(a.x, a.y, b.x, b.y, c.x, c.y);
+    if (area == 0 || !inside) return tuple{false, 0, 0, 0};
+    return { inside, e1 / area, e2 / area, e0 / area };
+}
